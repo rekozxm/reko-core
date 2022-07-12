0ae02a48dc5164f60205b4709ca401453123f547
MY ZHANG <ohzhangmy@qq.com>
2022-07-12 10:59:29 +0800

    feat(eslint):add eslint

--- app/main.tsx
+++ app/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ConfigProvider } from 'antd';
+import { ConfigProvider } from 'antd'
 import Layout from '@frame/layout'
 import '@styles/index.less'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ConfigProvider componentSize='small'>
+    <ConfigProvider componentSize="small">
       <Layout />
     </ConfigProvider>
   </React.StrictMode>

489a2b2c0a8feb45c458dd812c65b5f4270824ed
zhangmy <chuszzh@outlook.com>
2022-07-12 14:16:26 +0800

    feat(eslint):change default

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,17 @@ import { css, server, build, plugins } from './conf'
 export default defineConfig({
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, './'),
-      "@assets": path.resolve(__dirname, './frame/assets'),
-      "@styles": path.resolve(__dirname, './frame/styles'),
-      "@router": path.resolve(__dirname, './frame/router'),
-      "@types": path.resolve(__dirname, './frame/types'),
-      "@layout": path.resolve(__dirname, './frame/layout'),
-      "@atom": path.resolve(__dirname, './atom'),
-    },
+      '~': path.resolve(__dirname, './'),
+      '@assets': path.resolve(__dirname, './frame/assets'),
+      '@styles': path.resolve(__dirname, './frame/styles'),
+      '@router': path.resolve(__dirname, './frame/router'),
+      '@types': path.resolve(__dirname, './frame/types'),
+      '@layout': path.resolve(__dirname, './frame/layout'),
+      '@atom': path.resolve(__dirname, './atom')
+    }
   },
-  css, plugins, server, build
+  css,
+  plugins,
+  server,
+  build
 })
